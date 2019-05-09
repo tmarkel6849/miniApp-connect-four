59db0fe9e097edef56d7f7e0c7ac1726c2fc621f
Trevor Markel <tmarkel6849@gmail.com>
2019-05-09 10:01:56 -0600

    initial setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+const app = express();
+
+app.listen(port, (err) {
+  if (err) {
+    return console.error(err.messae);
+  }
+  console.log(`Express listening on port${port}`);
+});
+
+app.use(static, '/public');
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+})
+
+app.post('/game', (req, res) => {
+  res.sendStatus(200);
+})
