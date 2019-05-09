0e17d70e14d0786b459acb7d02870560bec35347
Trevor Markel <tmarkel6849@gmail.com>
2019-05-09 17:27:01 -0600

    toggle color working with react

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,17 +3,43 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = null;
-    this.placement = this.placement.bind(this);
+    this.state = {
+      row1: [],
+      row2: [],
+      row3: [],
+      row4: [],
+      row5: [],
+      row6: []
+    }
+    this.redPiece = this.redPiece.bind(this);
+    this.toggleColor = this.toggleColor.bind(this);
   }
 
-  placement() {
+  toggleColor() {
+    let currentColor = this.state.color;
+    this.setState({
+      color: currentColor === 'black' ? 'red' : 'black'
+    })
+    return currentColor;
+  }
 
+  redPiece(row, col) {
+    this.setState({
+      row: col
+    })
+  }
+
+  blackPiece(row, col) {
+    this.setState({
+      row: col
+    })
   }
 
   render() {
     return (
-      <div>Hello from react component</div>
+      <div>
+        <Board toggleColor={this.toggleColor}/>
+      </div>
     )
   }
 
@@ -25,22 +51,22 @@ class App extends React.Component {
 const Board = (props) => (
   <div>
     <table>
-    {datas.map(data => {
-      <Row someData={data}/>
-    })}
+      <tbody>
+        <Row toggleColor={props.toggleColor}/>
+      </tbody>
     </table>
   </div>
 )
 
 const Row = (props) => (
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
+  <tr className="row1">
+    <td className="1" onClick={()=>{$('.1').css('background-color', props.toggleColor())}}></td>
+    <td className="2"></td>
+    <td className="3"></td>
+    <td className="4"></td>
+    <td className="5"></td>
+    <td className="6"></td>
+    <td className="7"></td>
   </tr>
 );
 
